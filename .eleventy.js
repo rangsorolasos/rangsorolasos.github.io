0fe4aa0f623ac3da34855c8b543cf30e327f3b00
Arpad Magosanyi <m4gw4s@gmail.com>
2026-05-06 16:52:54 +0200

    deployment time in footer

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,16 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("site/styles.css");
+  eleventyConfig.addGlobalData("buildDate", new Date());
+eleventyConfig.addFilter("formatBuildDate", function(date) {
+    if (!date) date = new Date();
+    const pad = (n) => n.toString().padStart(2, '0');
+    const year = date.getFullYear();
+    const month = pad(date.getMonth() + 1);
+    const day = pad(date.getDate());
+    const hours = pad(date.getHours());
+    const minutes = pad(date.getMinutes());
+    return `${year}-${month}-${day} ${hours}:${minutes}`;
+  });
 
   return {
     dir: {
